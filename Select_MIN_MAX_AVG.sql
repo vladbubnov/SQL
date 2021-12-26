4e40152264d4bf4aa0ccf53b7abe2d8fef801261
Vlad Bubnov <vbubnov7@mail.ru>
2021-12-26 21:38:05 +0200

    Update file

--- Select_MIN_MAX_AVG.sql
+++ Select_MIN_MAX_AVG.sql
@@ -17,3 +17,29 @@ where unit_price > 30;
 select AVG(shipped_date - order_date)
 from orders
 where ship_country = 'USA';
+
+select order_id, customer_id, freight, ship_country
+from orders
+where ship_country like 'N%'
+order by freight desc 
+limit 10;
+
+select first_name, last_name, home_phone, region
+from employees 
+where region is null;
+
+select count(*)
+from customers
+where region is not null;
+
+select country, count(*)
+from suppliers
+group by country
+order by count(*) desc;
+
+select ship_country, sum(freight)
+from orders 
+where ship_country is not null
+group by ship_country
+having sum (freight) > 2750
+oerder by sum(freight) desc
