6a7157a57cad67cf08e1ab93c724e47bd021e427
Vlad Bubnov <vbubnov7@mail.ru>
2021-12-29 20:06:08 +0200

    Update SQL_DDL_part1

--- SQL_DDL_part1.sql
+++ SQL_DDL_part1.sql
@@ -1,4 +1,4 @@
---1. Создать таблицу employees 
+--1. Create a table employees 
 
 create table employees(
 	id serial primary key,
@@ -7,7 +7,7 @@ create table employees(
 
 select * from employees;
 
---2. Наполнить таблицу employees 70 строками
+--2. Add to the table employees 70 lines
 
 insert into employees(employee_name)
 	values ('Adam'),
@@ -81,7 +81,7 @@ insert into employees(employee_name)
 	 	   ('John'),
 	 	   ('Mila');
 	 	  
---3. Создать таблицу salary
+--3. Create a table salary
 	 	  
 create table salary(
 	id serial primary key,
@@ -90,7 +90,7 @@ create table salary(
 
 select * from salary;
 
---4. Наполнить таблицу salary 15 строками
+--4. Add to the table salary 16 lines
 
 insert into salary(monthly_salary)
 	values (1000),
@@ -110,7 +110,7 @@ insert into salary(monthly_salary)
 	       (2400),
 	       (2500);
 
---5. Создать таблицу employee_salary
+--5. Create a table employee_salary
 	      
 create table employee_salary(
 	id serial primary key,
@@ -120,7 +120,7 @@ create table employee_salary(
 
 select * from employee_salary;
 
---6. Наполнить таблицу employee_salary 40 строками
+--6. Add to the table employee_salary 40 lines
 
 insert into employee_salary(employee_id, salary_id)
 	values (3, 7),
@@ -164,7 +164,7 @@ insert into employee_salary(employee_id, salary_id)
 	 	   (39, 7),
 	 	   (47, 3);
 
---7. Создать таблицу roles 
+--7. Create a table roles 
 	 
 create table roles(
 	id serial primary key,
@@ -173,11 +173,95 @@ create table roles(
 
 select * from roles;
 
---8. Поменять тип столба role_name с int на varchar(30)
+--8. Сhange the column type role_name int to varchar(30)
 
 alter table roles
 alter column role_name type varchar(30);
 
---9. Наполнить таблицу roles 20 строками
+--9. Add to the table roles 20 lines
+
+insert into roles(role_name)
+	values ('Junior Python developer'),
+		   ('Middle Python developer'),
+		   ('Senior Python developer'),
+		   ('Junior Java developer'),
+		   ('Middle Java developer'),
+		   ('Senior Java developer'),
+		   ('Junior JavaScript developer'),
+		   ('Middle JavaScript developer'),
+		   ('Senior JavaScript developer'),
+		   ('Junior Manual QA engineer'),
+		   ('Middle Manual QA engineer'),
+		   ('Senior Manual QA engineer'),
+		   ('Project manager'),
+		   ('Designer'),
+		   ('HR'),
+		   ('CEO'),
+		   ('Sales manager'),
+		   ('Junior Automation QA engineer'),
+		   ('Middle Automation QA engineer'),
+		   ('Senior Automation QA engineer');
+		  
+select * from roles;
+
+--10. Create a table roles_employee
+
+create table roles_employee( 
+	id serial primary key,
+	employee_id int not null unique,
+	role_id int not null,
+	foreign key (employee_id)
+		references employees(id),
+	foreign key (role_id)
+		references roles(id)
+);
+
+--11. Add to the table roles_employee 40 lines
+
+insert into roles_employee(employee_id, role_id)
+	values (7, 2),		
+		   (20, 4),
+		   (3, 9),
+		   (5, 13),
+		   (23, 4),
+		   (11, 2),
+		   (10, 9),
+		   (22, 13),
+		   (21, 3),
+		   (34, 4),
+		   (6, 7),
+		   (1, 6),
+		   (2, 15),
+		   (4, 11),
+		   (8, 4),
+		   (9, 12),
+		   (13, 7),
+		   (14, 1),
+		   (15, 8),
+		   (17, 12),
+		   (18, 13),
+		   (19, 5),
+		   (24, 2),
+		   (25, 6),
+		   (35, 4),
+		   (36, 9),
+		   (40, 15),
+		   (33, 8),
+		   (69, 3),
+		   (41, 9),
+		   (42, 14),
+		   (43, 11),
+		   (47, 5),
+		   (48, 7),
+		   (50, 10),
+		   (51, 11),
+		   (55, 14),
+		   (60, 3),
+		   (65, 2),
+		   (68, 13);
+
+select * from roles_employee;
+
+
 
 
